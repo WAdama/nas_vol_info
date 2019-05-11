0905ffbb094ccde1e8b895e0ddc74f2cd253bbb4
Ingo <push@asche-rz.de>
2019-05-11 19:02:17 +0200

    Create nas_vol_info.sh

---
+++ nas_vol_info.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+VOL=$1
+IFS=" " read FS TOTAL USED AVAIL PERC VOLNAME <<< `df -k /$VOL | grep /$VOL`
+PERC=`echo $PERC | sed 's/%//g'`
+echo "<?xml version=\"10.0\" encoding=\"UTF-8\" ?><prtg><result><channel>Total</channel><value>$(($TOTAL*1024))</value><unit>BytesDisk</unit><VolumeSize>GigaByte</VolumeSize><float>1</float></result><result><channel>Used</channel><value>$(($USED*1024))</value><unit>BytesDisk</unit><VolumeSize>GigaByte</VolumeSize><float>1</float></result><result><channel>Available</channel><value>$(($AVAIL*1024))</value><unit>BytesDisk</unit><VolumeSize>GigaByte</VolumeSize><float>1</float></result><result><channel>Used (%)</channel><value>$PERC</value><unit>Percent</unit><LimitMode>1</LimitMode><LimitMaxWarning>90</LimitMaxWarning><LimitMaxError>95</LimitMaxError></result>"
+echo "<text>Volume: $VOL</text></prtg>"
+exit
