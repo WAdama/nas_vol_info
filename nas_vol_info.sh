b104cf0c4900ba893c339a40ea4d432fdf0e6c73
Ingo <foren@asche-rz.de>
2019-07-15 13:08:17 +0200

    A little optimizing...

--- nas_vol_info.sh
+++ nas_vol_info.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-# Version 1.0
+# Version 1.0.1
 
 VOL=$1
 IFS=" " read FS TOTAL USED AVAIL PERC VOLNAME <<< `df -k /$VOL | grep /$VOL`
 PERC=`echo $PERC | sed 's/%//g'`
-echo "<?xml version=\"10.0\" encoding=\"UTF-8\" ?><prtg><result><channel>Total</channel><value>$(($TOTAL*1024))</value><unit>BytesDisk</unit><VolumeSize>GigaByte</VolumeSize><float>1</float></result><result><channel>Used</channel><value>$(($USED*1024))</value><unit>BytesDisk</unit><VolumeSize>GigaByte</VolumeSize><float>1</float></result><result><channel>Available</channel><value>$(($AVAIL*1024))</value><unit>BytesDisk</unit><VolumeSize>GigaByte</VolumeSize><float>1</float></result><result><channel>Used (%)</channel><value>$PERC</value><unit>Percent</unit><LimitMode>1</LimitMode><LimitMaxWarning>90</LimitMaxWarning><LimitMaxError>95</LimitMaxError></result>"
+echo "<?xml version=\"10.0\" encoding=\"UTF-8\" ?><prtg><result><channel>Total</channel><value>$(($TOTAL*1024))</value><unit>BytesDisk</unit><VolumeSize>GigaByte</VolumeSize></result><result><channel>Used</channel><value>$(($USED*1024))</value><unit>BytesDisk</unit><VolumeSize>GigaByte</VolumeSize></result><result><channel>Available</channel><value>$(($AVAIL*1024))</value><unit>BytesDisk</unit><VolumeSize>GigaByte</VolumeSize></result><result><channel>Used (%)</channel><value>$PERC</value><unit>Percent</unit><LimitMode>1</LimitMode><LimitMaxWarning>90</LimitMaxWarning><LimitMaxError>95</LimitMaxError></result>"
 echo "<text>Volume: $VOL</text></prtg>"
 exit
