d63823757ab8b3a8c9e43dd0a0e46642fe2da448
Ingo <foren@asche-rz.de>
2020-05-30 18:14:42 +0200

    Minor change for better detection

--- nas_vol_info_b.sh
+++ nas_vol_info_b.sh
@@ -5,16 +5,16 @@
 # https://github.com/psi-4ward/check_btrfs_usage
 
 VOL=$1
-USAGE=`btrfs filesystem usage -b /$VOL/`
-SIZE=$(echo ""$USAGE"" | grep -F "Device size" | awk '{match($0,"([0-9]+)",a)}END{print a[1]}')
-ALLO=$(echo "$USAGE" | grep -F "Device allocated" | awk '{match($0,"([0-9]+)",a)}END{print a[1]}')
-UNAL=$(echo "$USAGE" | grep -F "Device unallocated" | awk '{match($0,"([0-9]+)",a)}END{print a[1]}')
-MISS=$(echo "$USAGE" | grep -F "Device missing" | awk '{match($0,"([0-9]+)",a)}END{print a[1]}')
-USED=$(echo "$USAGE" | grep -F "    Used" | awk '{match($0,"([0-9]+)",a)}END{print a[1]}')
-FREE=$(echo "$USAGE" | grep -F "Free (estimated)" | awk '{match($0,"([0-9]+)",a)}END{print a[1]}')
+USAGE=`btrfs filesystem usage -T -b /$VOL/`
+SIZE=$(echo ""$USAGE"" | grep -F "Device size:" | awk '{match($0,"([0-9]+)",a)}END{print a[1]}')
+ALLO=$(echo "$USAGE" | grep -F "Device allocated:" | awk '{match($0,"([0-9]+)",a)}END{print a[1]}')
+UNAL=$(echo "$USAGE" | grep -F "Device unallocated:" | awk '{match($0,"([0-9]+)",a)}END{print a[1]}')
+MISS=$(echo "$USAGE" | grep -F "Device missing:" | awk '{match($0,"([0-9]+)",a)}END{print a[1]}')
+USED=$(echo "$USAGE" | grep -F "Used:" | awk '{match($0,"([0-9]+)",a)}END{print a[1]}')
+FREE=$(echo "$USAGE" | grep -F "Free (estimated):" | awk '{match($0,"([0-9]+)",a)}END{print a[1]}')
 FMIN=$(echo "$USAGE" | grep -F "min:" | awk '{match($0,"min: ([0-9]+)",a)}END{print a[1]}')
-RESE=$(echo "$USAGE" | grep -F "Global reserve" | awk '{match($0,"([0-9]+)",a)}END{print a[1]}')
-RUSE=$(echo "$USAGE" | grep -F "Global reserve" | awk '{match($0,"used: ([0-9]+)",a)}END{print a[1]}')
+RESE=$(echo "$USAGE" | grep -F "Global reserve:" | awk '{match($0,"([0-9]+)",a)}END{print a[1]}')
+RUSE=$(echo "$USAGE" | grep -F "Global reserve:" | awk '{match($0,"used: ([0-9]+)",a)}END{print a[1]}')
 PERC=$(awk "BEGIN { pc=100*${USED}/${SIZE}; i=int(pc); print (pc-i<0.5)?i:i+1 }")
 echo "<?xml version=\"10.0\" encoding=\"UTF-8\" ?><prtg>
 <result><channel>Device size</channel><value>$SIZE</value><unit>BytesDisk</unit><VolumeSize>GigaByte</VolumeSize></result>
